bc32a11c9899646c402b31ca21cc08a2d8cd18f9
Claire Lemonnier <claire.lemonnier@pwc.com>
2021-11-17 11:59:59 +0000

    Add technician, action and reducer

--- src/components/techs/AddTechModal.js
+++ src/components/techs/AddTechModal.js
@@ -1,7 +1,10 @@
 import React, {useState} from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addTech } from '../../actions/techActions';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const AddTechModal = () => {
+const AddTechModal = ({addTech}) => {
 
     const [ firstName, setFirstName ] = useState('');
     const [ lastName, setLastName] = useState('');
@@ -11,7 +14,11 @@ const AddTechModal = () => {
         if( firstName === '' || lastName === ''){
             M.toast({html: 'Please enter a first and last name'})
         } else {
-            console.log(firstName, lastName);
+            addTech({
+                firstName,
+                lastName
+            });
+            M.toast({html: `${firstName} ${lastName} was added as a tech`})
             // clear fieds
             setFirstName('');
             setLastName('');
@@ -43,5 +50,7 @@ const AddTechModal = () => {
         </div>
     )
 }
-
-export default AddTechModal
+AddTechModal.propTypes ={
+    addTech: PropTypes.func.isRequired,
+}
+export default connect(null, {addTech})(AddTechModal)
